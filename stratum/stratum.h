70f5524d5542354c38ed928b3a93a9c7bcb03618
Sakda Chutchawan <oddhome@gmail.com>
2018-12-03 22:32:21 +0700

    add binarium_hash_v1 algo

--- stratum/stratum.h
+++ stratum/stratum.h
@@ -161,6 +161,9 @@ void sha256_double_hash_hex(const char *input, char *output, unsigned int len);
 #include "algos/x18.h"
 #include "algos/x22i.h"
 #include "algos/x21s.h"
+
+#include "algos/binarium_hash_v1.h"
+
 #include "algos/xevan.h"
 #include "algos/hmq17.h"
 #include "algos/nist5.h"
