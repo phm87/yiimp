629305f5abf4aec0ef23c7abd9689863c16d5523
phm87 <31578435+phm87@users.noreply.github.com>
2019-11-24 17:12:57 +0100

    Update atomicdex-api_trading.php

--- web/yaamp/core/trading/atomicdex-api_trading.php
+++ web/yaamp/core/trading/atomicdex-api_trading.php
@@ -3,6 +3,7 @@
 {
 	if(!$OrderID) return;
  	// todo
+	 return;
 	 
         $res = atomicdex_api_user("cancel_order", array("uuid" => $OrderID));
         if($res && !isset($res->error)) {
@@ -18,32 +19,34 @@
 	$exchange = 'AtomicDEX';
 	$updatebalances = true;
  	if (exchange_get($exchange, 'disabled')) return;
- 	$data = atomicdex_api_user('my_balance', array('coin' => 'BTC'));
-	if (empty($data)) return;
+ 	$list = atomicdex_api_user('get_enabled_coins');
+        if (empty($list)) return;
  	$savebalance = getdbosql('db_balances', "name='$exchange'");
  	foreach($data as $balance)
 	{
-		if ($balance->code == 'BTC') {
-			if (is_object($savebalance)) {
-				$savebalance->balance = $balance->balance;
-				$savebalance->onsell = $balance->lockedBalance;
-				$savebalance->save();
-			}
-			continue;
-		}
+                $data = atomicdex_api_user('my_balance', array('coin' => $c->ticker));
+                if (empty($data)) return;
+                if ($c->ticker == 'BTC') {
+                        if (is_object($savebalance)) {
+                                $savebalance->balance = $data->balance;
+                                $savebalance->onsell = $data->locked_by_swaps;
+                                $savebalance->save();
+                        }
+                        continue;
+                }
  		if ($updatebalances) {
 			// store available balance in market table
 			$coins = getdbolist('db_coins', "symbol=:symbol OR symbol2=:symbol",
-				array(':symbol'=>$balance->code)
+				array(':symbol'=>$c->ticker))
 			);
 			if (empty($coins)) continue;
 			foreach ($coins as $coin) {
 				$market = getdbosql('db_markets', "coinid=:coinid AND name='$exchange'", array(':coinid'=>$coin->id));
 				if (!$market) continue;
-				$market->balance = $balance->balance;
-				$market->ontrade = $balance->lockedBalance;
-				$market->balancetime = time();
- 				$market->deposit_address = $balance->address;
+				$market->balance = $data->balance;
+                                $market->ontrade = $data->locked_by_swaps;
+                                $market->balancetime = time();
+                                $market->deposit_address = $c->address;
 				$market->save();
 			}
 		}
