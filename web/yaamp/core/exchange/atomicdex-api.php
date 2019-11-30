dae3a39e36e524a28ec24446a9b63e313d2ce793
phm87 <31578435+phm87@users.noreply.github.com>
2019-11-30 15:50:33 +0100

    Update atomicdex-api.php

--- web/yaamp/core/exchange/atomicdex-api.php
+++ web/yaamp/core/exchange/atomicdex-api.php
@@ -3,33 +3,42 @@
 function atomicdex_api_query($method, $params=array(), $returnType='object')
 {
 //    curl --url "http://127.0.0.1:7783" --data "{\"userpass\":\"$userpass\",\"method\":\"my_balance\",\"coin\":\"MORTY\"}"
-	require_once('/etc/yiimp/keys.php');
-	$uri = "http://127.0.0.1:7783";
-
-	$json_body = "{\"userpass\"=\"".EXCH_ATOMICDEX_RPCPASSWORD."\",\"method\":\"".$method."\"";
-	if ($method == "electrum")	{
-	  foreach ($params as $p => $v)   {
-    		$json_body .= ",";
-		if ($p == "servers")	{
-			$first = true;
-			$json_body .= "\"servers\":[";
-			foreach ($v as $pp => $vv)   {
-				if ($first == false)	$json_body .= ",";
-				$json_body .= "{\"url\":\"".$vv['url']."\"}";
-				$first = false;
-				}
-			$json_body .= "]";
-			}
-		  else
-	    		$json_body .= "\"$p\":\"$v\"";
-    		}
-	}
-	else	{
-		foreach ($params as $p => $v)   {
-    			$json_body .= ",";
-    			$json_body .= "\"$p\":\"$v\"";
-    			}
-	}
+        require_once('/etc/yiimp/keys.php');
+        $uri = "http://127.0.0.1:7783";
+        $json_body = "{\"userpass\":\"".EXCH_ATOMICDEX_RPCPASSWORD."\",\"method\":\"".$method."\"";
+        if ($method == "electrum" || $method == "cancel_all_orders")    {
+          foreach ($params as $p => $v)   {
+                $json_body .= ",";
+                if ($p == "servers" || $p == "cancel_by")       {
+//debuglog("servers");
+                        $first = true;
+                        $json_body .= "\"".$p."\":";
+                        if ($method == "electrum")
+                                $json_body .= "[";
+                        foreach ($v as $pp => $vv)   {
+                                if ($first == false)    $json_body .= ",";
+                                if ($method == "electrum")
+                                        $json_body .= "{\"url\":\"".$vv['url']."\"}";
+                                else
+                                        $json_body .= "{\"".$pp."\":\"".$vv."\"}";
+                                $first = false;
+                                }
+                        if ($method == "electrum")
+                        	$json_body .= "]";
+                        }
+                  else
+                        $json_body .= "\"$p\":\"$v\"";
+                }
+        }
+        else    {
+                foreach ($params as $p => $v)   {
+                        $json_body .= ",";
+                        if ($p == "cancel_previous")
+                                $json_body .= "\"$p\":$v";
+                        else
+                                $json_body .= "\"$p\":\"$v\"";
+                        }
+        }
   $json_body .= "}";
   $ch = curl_init($uri);
   curl_setopt($ch, CURLOPT_POST, 1);
@@ -45,8 +54,7 @@ function atomicdex_api_query($method, $params=array(), $returnType='object')
   curl_close($ch);
   return $res;
 }
-
 function atomicdex_api_user($method, $url_params=array(), $json_body='')
 {
-	return atomicdex_api_query($method, $url_params);
+        return atomicdex_api_query($method, $url_params);
 }
